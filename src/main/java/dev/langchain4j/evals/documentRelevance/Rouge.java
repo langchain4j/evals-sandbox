353c41d2baa96f64ce8e38d8a0af788a3bf81b76
Dimitris Kafetzis <dimitris.kafetzis00@gmail.com>
2024-12-06 01:40:09 +0200

    Added ROUGE relevancy test

--- src/main/java/dev/langchain4j/evals/documentRelevance/Rouge.java
+++ src/main/java/dev/langchain4j/evals/documentRelevance/Rouge.java
@@ -3,12 +3,44 @@ package dev.langchain4j.evals.documentRelevance;
 
 import org.kie.trustyai.metrics.language.rouge.ROUGE;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rouge {
     public static void main(String[] args) {
-        String sentence1 = "The quick brown fox jumps over the lazy dog.";
-        String sentence2 = "The quick brown fox jumps over the lazy frog.";
+        ROUGE RougeScorer = new ROUGE(ROUGE.RougeTypes.ROUGEL);
 
-        ROUGE RougeScorer = new ROUGE(ROUGE.RougeTypes.ROUGE_LSUM);
-        System.out.println(RougeScorer.calculate(sentence1, sentence2));
+        for (int i = 0; i < Samples.sampleGroundTruths.size(); i++) {
+            System.out.println("Run: " + i);
+
+            //Get documents for the scenario and replace any file paths with the content they point to
+            List<String> groundTruths = Samples.sampleGroundTruths.get(i).stream().map(Samples::fileToContent).map(String::toLowerCase).toList();
+            List<String> retrievedDocuments = Samples.sampleRetrievedDocuments.get(i).stream().map(Samples::fileToContent).map(String::toLowerCase).toList();
+
+
+            List<String> relevantDocuments = new ArrayList<>();
+
+            for (String retrievedDocument : retrievedDocuments) {
+                double similarity = 0.0;
+                System.out.println("Testing Document:\n-------------------\n\n-------------------\n");
+                for (String groundTruthDocument : groundTruths) {
+                    double calculatedSimilarity = RougeScorer.calculate(groundTruthDocument, retrievedDocument);
+                    System.out.println("Calculated Similarity: " + calculatedSimilarity);
+                    if (calculatedSimilarity > similarity) {
+                        similarity = calculatedSimilarity;
+                    }
+                }
+                System.out.println("Highest Similarity: " + similarity);
+
+                //Similarity drops significantly when comparing big chunks of texts against small parts of them
+                //But the difference between relevant and not relevant can still be seen
+                if (similarity >= 0.09) {
+                    System.out.println("Document is deemed as relevant, adding");
+                    relevantDocuments.add(retrievedDocument);
+                }
+            }
+            System.out.println("Relevant documents:" + relevantDocuments.size());
+            System.out.println("All retrieved:" + retrievedDocuments.size());
+        }
     }
 }
