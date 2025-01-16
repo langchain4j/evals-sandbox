80eeccdafde37c09ba8296ca2736fa06c0b35962
Dimitris Kafetzis <dimitris.kafetzis00@gmail.com>
2025-01-16 21:46:56 +0200

    SentenceMatcher 2.0

--- src/test/java/dev/langchain4j/evals/SentenceMatcher.java
+++ src/test/java/dev/langchain4j/evals/SentenceMatcher.java
@@ -33,6 +33,7 @@ public class SentenceMatcher implements Matcher{
         //It is very effective but strict, in order for the groundTruth string to be a match, every sentence in it must be contained in the list of retrieved strings
         //Really this calculates recall in its current configuration since we are trying to match a ground truth string against a list of retrieved strings
         //But precision can be calculated by flipping the arguments of the method (providing A retrieved string and trying to match it against a list of ground truths)
+        //If the criteria is too strict, we can add a threshold that the user has access to determine how many sentences should be found before we consider it a match
 
         Set<String> groundTruthSentences = Set.of(sentenceDetector.sentDetect(groundTruth));
         for (String sentence : groundTruthSentences){
